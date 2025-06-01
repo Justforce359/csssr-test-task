0e5f127a94594e07c6e8dacd7daa4d6b1baa68aa
Mikhail Ustinov <mikhailu359@gmail.com>
2025-06-01 10:08:48 +0200

    Initial commit with first tests

---
+++ src/test/java/CsssrTest.java
@@ -0,0 +1,46 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+public class CsssrTest {
+
+    @BeforeEach
+    void startUp () {
+        open("https://csssr.github.io/qa-engineer/");
+    }
+    @Disabled("Баг: кнопка 'Отправить' остаётся активной при незаполненной форме")
+    @Test
+    @DisplayName("Send button should be inactive when form fields are empty")
+    void sendButtonShouldBeInactiveIfFormIsEmptyTest () {
+        $(".email").shouldBe(empty);
+        $(".description").shouldBe(empty);
+        $x(".//button[@type='submit']").scrollTo().shouldNotBe(enabled);
+    }
+
+    SelenideElement beautifulInput = $("#beautiful");
+    SelenideElement beautifulInputLabel = $("label[for='beautiful']");
+
+    @Disabled("Баг: чекбокс 'Чувство прекрасного' не ставится обратно")
+    @Test
+    @DisplayName("Bug checkbox should toggle correctly")
+    void checkboxShouldToggleBack () {
+        open("https://csssr.github.io/qa-engineer/");
+        $(byText("НАХОДИТЬ НЕСОВЕРШЕНСТВА")).click();
+        beautifulInputLabel.scrollTo();
+        beautifulInput.shouldBe(selected);
+        beautifulInputLabel.click();
+        beautifulInput.shouldNotBe(selected);
+        beautifulInputLabel.click();
+        beautifulInput.shouldBe(selected);
+
+    }
+}
