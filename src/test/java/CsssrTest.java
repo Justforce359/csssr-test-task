8bc09ac73fcfac0207652a0643267504e4ff6c8b
Justforce359 <131783996+Justforce359@users.noreply.github.com>
2025-06-01 11:00:33 +0200

    Update CsssrTest.java

--- src/test/java/CsssrTest.java
+++ src/test/java/CsssrTest.java
@@ -1,15 +1,11 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class CsssrTest {
 
